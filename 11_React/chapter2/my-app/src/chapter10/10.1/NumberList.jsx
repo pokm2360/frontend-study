889829a76f800eeb2019440f45510f9e22424e30
pokm <por0632@naver.com>
2024-05-02 16:54:55 +0900

    5/2 수업

---
+++ 11_React/chapter2/my-app/src/chapter10/10.1/NumberList.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const numbers = [1,2,3,4,5];
+
+const NumberList = (props) => {
+  // 배열을 반복 렌더링 할때는 key 속성을 필수로 넣어야 함
+  // 임시로 배열의 index를 넣어 key 누락 문제를 해결
+  const listItems = numbers.map((number, index) => {
+    return <li key={index}>{number}</li>
+  });
+
+  return (
+    <>
+      <ul>
+        {listItems}
+      </ul>
+
+      <ul>
+        {numbers.map((number, index) => {
+          return <li key={index}>{number}</li>;
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default NumberList;

612243779b82c6203e008726f46bc731fd3fafb9
pokm2360 <por0632@naver.com>
2024-06-20 14:58:46 +0900

    서버

--- 11_React/my-shop/src/api/productAPI.js
+++ 11_React/my-shop/src/api/productAPI.js
@@ -23,4 +23,35 @@ export const getMoreProducts = async () => {
   }
 };
 
-//
+// 만약 서버로 데이터를 보내야 한다면??
+// json-server 이용 시 테스트 가능
+export const addProduct = async (product) => {
+  try {
+    const response = await axios.post('http://localhost:4000/products', { product });
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      throw new Error(`api error: ${response.status} ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+// 개념 정리: 서버와 데이터를 주고 받을 때, 텍스트(문자)만 주고 받을 수 있음
+// array, object 같은 데이터 타입은 서버가 모름 그래서 JSON이라는 포맷으로 변환해서 보냄
+
+// 인자값을 객체로 넘기고 받으면 매개변수의 순서 신경안써도 되고 중간에 인자값이 빠져도 오케이
+export const orderProduct = async ({ productId, orderCount }) => {
+  try {
+    const response = await axios.post('http://localhost:4000/product-order', { productId, orderCount });
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      throw new Error(`api error: ${response.status} ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};

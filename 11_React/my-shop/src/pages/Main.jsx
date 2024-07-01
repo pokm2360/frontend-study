9419a54257fadee00e3ca058958b7ccaeaf53642
pokm2360 <por0632@naver.com>
2024-07-01 15:03:34 +0900

    7/1

--- 11_React/my-shop/src/pages/Main.jsx
+++ 11_React/my-shop/src/pages/Main.jsx
@@ -1,39 +1,45 @@
+import { useDebugValue, useEffect } from "react";
 import styled from "styled-components";
 import { Button, Col, Container, Row } from "react-bootstrap";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { PulseLoader } from "react-spinners";
+
 import { addMoreProducts, getAllProducts, getMoreProductsAsync, selectProductList, selectStatus } from "../features/product/productSlice";
 import ProductListItem from "../components/ProductListItem";
 
 // 리액트(JS)에서 이미지 파일 가져오기
 // 1) src 폴더 안 이미지(상대 경로로 import해서 사용)
-import bgImg from "../images/yonex.jpg";
+import yonexImg from "../images/yonex.jpg";
 import { getMoreProducts } from "../api/productAPI";
-import { MoonLoader, SyncLoader } from "react-spinners";
 import RecentProducts from "../components/RecentProducts";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { logoutSuccess } from "../features/user/userSlice";
 // 2) public 폴더 안 이미지(root 경로로 바로 접근)
 // 빌드 시 src 폴더에 있는 코드와 파일은 압축이 되지만 public 폴더에 있는 것들은 그대로 보존
 // 이미지 같은 수정이 필요없는 static 파일의 경우 public에 보관하기도 함
 
 const MainBackground = styled.div`
   height: 500px;
-  background-image: url("/images/yonex.jpg");
+  background-image: url(${yonexImg});
+  /* background-image: url("/images/yonex.jpg"); */
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
 `;
 
 function Main() {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const productList = useSelector(selectProductList);
-  const status = useSelector(selectStatus); // api 요청 상태(로딩 상태)
+  const status = useSelector(selectStatus); // API 요청 상태(로딩 상태)
 
   // 처음 마운트 됐을 때 서버에 상품 목록 데이터를 요청하고
   // 그 결과를 리덕스 스토어에 전역 상태로 저장
   useEffect(() => {
     // 서버에 상품 목록 요청
-    axios.get('https://my-json-server.typicode.com/pokm2360/db-shop/products')
+    axios.get('https://my-json-server.typicode.com/geoblo/db-shop/products')
       .then((response) => {
         console.log(response.data);
         dispatch(getAllProducts(response.data));
@@ -45,7 +51,6 @@ function Main() {
 
   const handleGetMoreProducts = async () => {
     const result = await getMoreProducts();
-    console.log(result);
     dispatch(addMoreProducts(result));
   };
 
@@ -66,43 +71,25 @@ function Main() {
           <Row>
             {/* 부트스트랩 이용한 반응형 작업 */}
             {/* md >= 768px 이상에서 전체 12등분 중 4:4:4로 보여줌 */}
-          {/* <Col md={4} sm={6}>
-            <img src="https://www.yonexmall.com/shop/data/goods/1645767865278s0.png" width="80%" />
-            <h4>상품명</h4>
-            <p>상품가격</p>
-          </Col>
-          <Col md={4} sm={6}>
-            <img src="https://www.yonexmall.com/shop/data/goods/1659329583483s0.png" width="80%" />
-            <h4>상품명</h4>
-            <p>상품가격</p>
-          </Col>
-          <Col md={4} sm={6}>
-            <img src="https://www.yonexmall.com/shop/data/goods/1667190100104s0.png" width="80%" />
-            <h4>상품명</h4>
-            <p>상품가격</p>
-          </Col> */}
-
-          {/* ProductListItem 컴포넌트를 만들어서 반복 렌더링으로 바꾸고 데이터 바인딩 */}
-          {/* Quiz: 
+            {/* ProductListItem 컴포넌트를 만들어서 반복 렌더링으로 바꾸고 데이터 바인딩 */}
+            {/* Quiz: 
               1) 반복적인 상품 아이템을 src/components/ProductListItem 컴포넌트로 만들기
               2) productList 배열을 반복하며 ProductListItem 컴포넌트를 렌더링 하기
               3) 상품 정보를 props로 넘겨서 데이터 바인딩 하기
-          */}
-          {productList.map((product) => {
-            return <ProductListItem key={product.id} product={product}/>
-          })};
+            */}
+            {productList.map((product) => <ProductListItem key={product.id} product={product} />)}
 
-          {/* 로딩 만들기 */}
-          {status === 'loading' &&
-            <div>
-              <SyncLoader
-                color="#000000"
-                margin={10}
-                size={40}
-                speedMultiplier={2}
+            {/* 로딩 만들기 */}
+            {status === 'loading' &&
+              <PulseLoader
+                color="#36d7b7"
+                margin={50}
+                size={30}
+                cssOverride={{
+                  display: 'block'
+                }}
               />
-            </div>
-          }
+            }
           </Row>
         </Container>
 
@@ -111,8 +98,7 @@ function Main() {
           받아온 결과를 전역 상태에 추가하기 위해 리듀서 추가 및 액션 생성 함수 export
           스토어에 dispatch로 요청(액션) 보내기
         */}
-        <Button variant="secondary" className="mb-4" onClick=
-        {handleGetMoreProducts}>
+        <Button variant="secondary" className="mb-4" onClick={handleGetMoreProducts}>
           더보기
         </Button>
 
@@ -120,10 +106,35 @@ function Main() {
         <Button variant="secondary" className="mb-4" onClick={handleGetMoreProductsAsync}>
           더보기 {status}
         </Button>
+
+        {/* (테스트용) 게시물 목록 조회 */}
+        <Button variant="secondary" className="mb-4" onClick={async () => {
+          try {
+            const token = localStorage.getItem('token');
+            const response = await axios.get(`http://ec2-13-209-77-178.ap-northeast-2.compute.amazonaws.com:8080/board/list`, {
+              headers: {
+                Authorization: token
+              }
+            });
+            console.log(response.data);
+          } catch (error) {
+            console.log(error);
+            toast.error(error.response.data.message, {
+              position: 'top-center'
+            });
+
+            if (error.response.data.code === '403') {
+              dispatch(logoutSuccess());
+              navigate('/login');
+            }
+          }
+        }}>
+          게시물 조회
+        </Button>
       </section>
 
       {/* 최근 본 상품 컴포넌트 */}
-      {productList.length > 0 && <RecentProducts productList={productList}/>}
+      {productList.length > 0 && <RecentProducts productList={productList} />}
     </>
   );
 };
@@ -160,3 +171,4 @@ export default Main;
 // https://my-json-server.typicode.com/geoblo/db-shop
 // https://my-json-server.typicode.com/geoblo/db-shop/products
 // https://my-json-server.typicode.com/geoblo/db-shop/products/1
+

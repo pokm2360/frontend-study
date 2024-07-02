87aedcb08621ed1564d88d021905c21750b66e38
pokm2360 <por0632@naver.com>
2024-07-02 14:51:20 +0900

    ㅇㅇ

--- 11_React/my-app/src/chapter11/11.3/FileInput.jsx
+++ 11_React/my-app/src/chapter11/11.3/FileInput.jsx
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+import { axios } from "axios";
 
 function FileInput() {
   const fileInput = useRef(null);
@@ -6,7 +7,27 @@ function FileInput() {
     e.preventDefault();
     console.log(fileInput.current.files);
     alert(`선택된 파일${fileInput.current.files[0].name}`);
-  }
+
+    const formData = new FormData();
+    // formData.append('title', title);
+    // formData.append('content', content);
+    formData.append('file', fileInput.current.files[0] || null);
+
+    // 아래 두가지 방식 중에 택1
+    // 1) multipart/form-data 타입으로 보냄
+    axios.post('https://api-url', formData);
+
+    // 2) 헤더에 콘텐트 타입 설정 시 자동으로 직렬화
+    axios.post('https://api-url', {
+      // title,
+      // content, 
+      file: fileInput.current.files[0]
+    }, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    });
+  };
 
   // file input은 값을 설정 할 수 없고 사용자가 첨부한 파일의 정보만 읽어올 수 있기 때문에
   // 비제어 컴포넌트가 됨
